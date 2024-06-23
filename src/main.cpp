8d1f1c27114ef54774524a555753afbd44b4cfd7
Caleb <cjmeepers@gmail.com>
2024-06-23 16:39:13 +0800

    Create window (#1)
    * add glfw
    
    * Update main.cpp
    
    * Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,49 @@
 #include <iostream>
+#include <memory>
+
+#include <GLFW/glfw3.h>
+
+struct Window
+{
+	int width = 0;
+	int height = 0;
+
+	void* hwnd = nullptr;
+};
+
+typedef std::shared_ptr<Window> winptr;
+
+static void createWindow(int width, int height, winptr& window)
+{
+	window->hwnd = glfwCreateWindow(width, height, "Default Window", nullptr, nullptr);
+}
+
+static void destroyWindow(winptr& window)
+{
+	// assume hwnd is handle to GLFW window
+	glfwDestroyWindow(static_cast<GLFWwindow*>(window->hwnd));
+}
+
+static bool canCloseWindow(winptr& window)
+{
+	return glfwWindowShouldClose(static_cast<GLFWwindow*>(window->hwnd));
+}
 
 int main(int argc, char* argv[])
 {
+	if (glfwInit() != GLFW_TRUE)
+		perror("Error with glfw lol");
+
 	printf("%s\n", "Hello World!");
+
+	winptr window = std::make_shared<Window>(1920, 1080);
+	createWindow(window->width, window->height, window);
+
+	// this is your render loop
+	while (!canCloseWindow(window))
+	{
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
 }
